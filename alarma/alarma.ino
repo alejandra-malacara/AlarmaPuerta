1d66e85d827982362c9cd8215337cebc3f4c2094
alejandra-malacara <alejandra.malacaraavila@gmail.com>
2018-09-26 20:21:56 -0500

    Acomodar codigo

--- alarma/alarma.ino
+++ alarma/alarma.ino
@@ -1,47 +1,49 @@
-// libreria sleep nos permite que se reduzca el consumo de energia 
-// donde avr se puede poner en diferentes modos de suspension 
- #include <avr/sleep.h>
+
+ 
+#include <avr/sleep.h> // libreria sleep nos permite que se reduzca el consumo de energia 
+                       // en  avr se puede poner en diferentes modos de suspension 
  
 //Definimos pines 
 #define boton 2 
 #define buzzer 8
 #define led 13
+//variable 
+int volumen = 0;  //inicializar varible en cero 
 
-int volumen = 0; //inicializar varible en cero 
-
-void setup() {
-pinMode(boton,OUTPUT); //se declara pin 2 como salida 
-pinMode(buzzer,OUTPUT); //se declara pin 8 como salida 
-pinMode(led, OUTPUT);// se declara pin 13 como salid 
-set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+void setup(){
+pinMode(boton,OUTPUT);                //se declara pin 2 como salida 
+pinMode(buzzer,OUTPUT);               //se declara pin 8 como salida 
+pinMode(led, OUTPUT);                 // se declara pin 13 como salid 
+set_sleep_mode(SLEEP_MODE_PWR_DOWN);  //el mayor ahorro de energia, aqui se establece el modo de energia 
 attachInterrupt(digitalPinToInterrupt(boton),inter,RISING); // interrupccion en el boton 
-}
+}                                     //llave final de metodo setup
 
 
 void loop() {
-     if(volumen==0){ // si la variable volumen esta en 0, el modo sleep se activa 
-      digitalWrite(13, LOW); // led se apaga 
-     sleep_enable();// modo de suspension 
-    sleep_mode(); // Esta macro establece automáticamente el bit de habilitación de suspensión
-                   //se pone en suspensión y borra el bit de habilitación de suspensión.
-  }else{ //caso contrario,  estara en modo disable desabilita la suspension 
-  sleep_disable();// modo inhabilitar 
-  alarm(); //llama metodo alarma  
-  }
+  if(volumen==0){              // si la variable volumen esta en 0, el modo sleep se activa 
+      digitalWrite(led, LOW);// led se apaga 
+      sleep_enable();        // modo de suspension 
+      sleep_mode();          // Esta macro establece automáticamente el bit de habilitación de suspensión
+                             //se pone en suspensión 
+   }//fin de if 
+    else{                    //caso contrario,  estara en modo disable desabilita la suspension 
+      sleep_disable();       // modo inhabilitar, desactiva el modo suspencion 
+      alarm();               //llama metodo alarma  
+    }                        // fin else 
 }
 
-void inter(){ // Metodo para la interrupcion 
-volumen=!volumen; // si  volumen=true; después de la expresión volumen=!volumen ; entonces a tendrá el valor false o viceversa 
-  
-}
+
+void inter(){               // Metodo para la interrupcion 
+ volumen=!volumen;          // si  volumen=true; después de la expresión volumen=!volumen ; entonces a tendrá el valor false 
+ }
+
 
 void alarm(){
- analogWrite(buzzer,128);// analogWrite escribe un valor en un pin analógico
+ analogWrite(buzzer,128); // analogWrite escribe un valor en un pin analógico
                           // a pin buzzer le damos un ciclo de trabajo de 128
- delay(200);
- digitalWrite(13, HIGH);// se enciende led 
-  //delay(200);
- digitalWrite(buzzer,0);// el pin va a generar una onda constante del ciclo de trabajo especificado hasta la siguiente llamada
-//                            // en este caso con digitalWrite 
-   delay(100);
+ delay(200);              // espera por 200ms
+ digitalWrite(led, HIGH); // se enciende led 
+ digitalWrite(buzzer,0);  // el pin va a generar una onda constante del ciclo de trabajo especificado hasta la siguiente llamada
+                          // en este caso con digitalWrite 
+ delay(100);              // espera por 100ms 
 }
